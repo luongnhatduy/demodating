ada453b842580ab567782fd564e5b1ec0de4e3ec
Duy Lương <nhatduy2769@gmail.com>
2019-07-05 22:45:57 +0700

    dmm

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
       <Provider store={store}>
         <View>
           <TaskManagerComponent />
-          ///Aanh duy dep trai khoai to
+          ///Aanh duy dep trassssssssssssssssssss
         </View>
       </Provider>
     
