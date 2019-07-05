6160fcbe0fe75813ee675c98bd7d2556ad021cf8
Duy Lương <nhatduy2769@gmail.com>
2019-07-05 22:28:26 +0700

    sss

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import AddComponent from "./src/component/AddComponent";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import allReducers from "./src/redux/reducers/index";
+import TaskManagerComponent from "./src/component/taskManagerComponent";
+
+let store = createStore(allReducers);
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <View>
+          <TaskManagerComponent />
+        </View>
+      </Provider>
+    
+    );
+  }
+}
